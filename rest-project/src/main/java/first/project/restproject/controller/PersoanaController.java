35bfbef54569a8cefb0d5a4fc5e1a692ce063a4e
tomac <tomac@DESKTOP-35KL8TN.local>
2021-02-01 10:21:55 +0200

    erori

--- rest-project/src/main/java/first/project/restproject/controller/PersoanaController.java
+++ rest-project/src/main/java/first/project/restproject/controller/PersoanaController.java
@@ -1,14 +1,19 @@
 package first.project.restproject.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import first.project.restproject.entity.Persoana;
+import first.project.restproject.service.PersoanaService;
 
 
 
 
-persoanaService.getAllPerssons()
+
+
 
 
 @RestController
